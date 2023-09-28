8c57c3121d0f18a96cfe4ae12266334ffdde4b5f
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2023-09-28 03:24:22 -0700

    update

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ productList.classList.add('product-container');
 productList.innerHTML = market;
 productWrapper.appendChild(productList)
 
-//add to cart
+//add to cart or create new order
 const addToCartBtn = document.querySelectorAll('.add-to-cart');
 addToCartBtn.forEach(btn => {
 
@@ -39,16 +39,29 @@ btn.addEventListener('click', async (e) => {
         e.preventDefault();
         //code 
         let product_id = btn.id;
-        console.log('product id')
+        // console.log('product id')
         console.log(product_id)
         console.log("customer_id")
+
+
+        let customerId = localStorage.getItem('customer_id');
+
+        //if customer id is not found then create it
+        if(customerId == null || customerId == ''){
+            createNewCustomerId();
+            return;
+        }
+
         let customer_id = localStorage.getItem('customer_id');
+
         console.log(customer_id)
+
+
         console.log('product price')
         let mainEL = e.target.parentElement;
         const price = mainEL.querySelector('p.price')
         const priceVal = price.innerHTML;
-        console.log(priceVal)
+        // console.log(priceVal)
 
 
         alert('attempting to add new item to shopping cart' + product_id)
@@ -60,12 +73,16 @@ btn.addEventListener('click', async (e) => {
                 "content-type" : "application/json "
             },
             body: JSON.stringify({
-                product_id: +product_id,
+                product_id: +product_id, //Number(product_id)
                 customer_id: +customer_id,
-                price: +priceVal
+                price: +priceVal //Number(priceVal)
             })
 
         }) 
+        if(newOrder.status == 409){
+            alert("this product has already been added to cart")
+            return;
+        }
         if(newOrder.status == 200){
             let res = newOrder.json();
             console.log(res)
@@ -84,9 +101,10 @@ const createNewCustomerId = async () => {
 //get customer token
 let customer_token = localStorage.getItem('customer_token');
 
-if (!customer_token){
+if (customer_token == null || customer_token == ''){
     customer_token = Math.random() +  new Date().toLocaleDateString()
     console.log("customer token")
+
     console.log(customer_token)
     //create new customer
 
@@ -113,37 +131,47 @@ console.log('customer already created')
 
 createNewCustomerId();
 
-const cartNumber = async (customer_token) => {
+const cartNumber = async () => {
 console.log("getting cart items list")
+let customerToken = localStorage.getItem('customer_token')
+
+//if customer is not in localstorage then, store again
+if(customerToken == null || customerToken == ''){
+    createNewCustomerId();
+}
+
+
+console.log("attempting to send customer token")
+console.log(customerToken)
+
 //get cart items number
-// setInterval(() => {
+
   const result =  await  fetch('http://localhost:9222/shop/v1/customer-with-token',{
     method: 'POST',
     headers: {
                 "content-type" : "application/json"
             },
             body: JSON.stringify({
-                token : customer_token
+                token : customerToken
             })
   })
   if(result.status == 200){
     let response = await result.json();
-    console.log(response)
-    console.log("response id")
+    // console.log(response)
+    // console.log("response id")
     const id = response[0].id;
-    console.log(id) 
+    // console.log(id) 
 
     //store customer id in localstorage
    let customer_id = localStorage.getItem('customer_id');
 
-   if(!customer_id){
+   if(customer_id == null || customer_id == ''){
     localStorage.setItem('customer_id', id);
    }
-
-    
+ 
     //get orders with customer id 
 
-    const rs = await fetch('http://localhost:9222/shop/v1/orders-with-customerId',{
+    const rs = await fetch('http://localhost:9222/shop/v1/orders-with-customerId', {
         method: 'POST',
         headers: {
             "content-type" : "application/json"
@@ -152,12 +180,14 @@ console.log("getting cart items list")
             customer_id : id
         })
 
-        });
+     });
 
     if(rs.status == 200){
-        let orders = await rs.json();
+        let orderArray = await rs.json();
         console.log(orders)
-        const {order } = orders;
+
+        //deconstruct order from array
+        const {order } = orderArray;
         const orderCount = order.reduce((count, items)=> {
             return count + 1;
         }, 0)
@@ -169,16 +199,14 @@ console.log("getting cart items list")
     }
 
 
-
   }
     
-// }, 1500);
+
 }
 
 // setInterval(() => {
     
 
-let customerToken = localStorage.getItem('customer_token')
-cartNumber(customerToken);
+cartNumber();
 
 // }, 2000);
